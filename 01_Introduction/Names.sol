3a5ba1afa181aaf1e1017536a6e8b7a7a6277563
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-02-07 17:39:18 +0300

    Ders 1

---
+++ 01_Introduction/Names.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Names is Ownable {
+    string public ownerName;
+    string[] public names;
+    int256 private luckyNumber;
+
+    constructor(string memory _ownerName, int256 _luckyNumber) {
+        ownerName = _ownerName;
+        names.push(_ownerName);
+        luckyNumber = _luckyNumber;
+    }
+
+    function addMyName(string memory _myName) public {
+        names.push(_myName);
+    }
+
+    function isAdded(string memory _name) public view returns (bool) {
+        for (uint256 i = 0; i < names.length; i++) {
+            if (
+                keccak256(abi.encodePacked((_name))) ==
+                keccak256(abi.encodePacked((names[i])))
+            ) return true;
+        }
+        return false;
+    }
+
+    function getNames() public view returns (string[] memory) {
+        return names;
+    }
+
+    function nameCount() public view returns (uint256) {
+        return names.length;
+    }
+
+    function changeOwnersName(string memory _ownerName) public onlyOwner {
+        ownerName = _ownerName;
+    }
+}
---
+++ 01_Introduction/README.md
@@ -0,0 +1,13 @@
+# Introduction
+
+# Resources
+
+[Web3 Uygulamaları Makalesi](https://blog.ibby.dev/introduction-to-web3-development)
+[How Does Ethereum Work, Anyway?](https://www.preethikasireddy.com/post/how-does-ethereum-work-anyway)
+[Javascript Eğitimimiz](https://www.youtube.com/playlist?list=PLby2HXktGwN5WnvvI3jm0xSaac79JyRpT)
+[Metamask](https://www.metamask.io)
+[Metamask Ağ Ekleme](htttps://www.chainlist.org)
+[Metamask - Avalanche](https://support.avax.network/en/articles/4626956-how-do-i-set-up-metamask-on-avalanche)
+[Solidity Docs](https://docs.soliditylang.org/en/latest/)
+[Remix IDE](https://www.remix.ethereum.org)
+[Names Kontrat Örneği](./Names.sol)
