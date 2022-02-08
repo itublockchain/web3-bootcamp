3c0749756249dba7eab07cf150c679f697f73e27
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-02-08 03:05:57 +0300

    mappings

---
+++ 0X+1_Mappings/Mapping.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract Mapping {
+    mapping(address => bool) public registered;
+    mapping(address => int256) public favNums;
+
+    function register(int256 _favNum) public {
+        //require(!registered[msg.sender], "Kullanıcınız daha önce kayıt yaptı.");
+        require(!isRegistered(), "Kullaniciniz daha once kayit yapti.");
+        registered[msg.sender] = true;
+        favNums[msg.sender] = _favNum;
+    }
+
+    function isRegistered() public view returns (bool) {
+        return registered[msg.sender];
+    }
+
+    function deleteRegistered() public {
+        require(isRegistered(), "Kullaniciniz kayitli degil.");
+        delete (registered[msg.sender]);
+        delete (favNums[msg.sender]);
+    }
+}
+
+contract NestedMapping {
+    mapping(address => mapping(address => uint256)) public debts;
+
+    function incDebt(address _borrower, uint256 _amount) public {
+        debts[msg.sender][_borrower] += _amount;
+    }
+
+    function decDebt(address _borrower, uint256 _amount) public {
+        require(debts[msg.sender][_borrower] >= _amount, "Not enough debt.");
+        debts[msg.sender][_borrower] -= _amount;
+    }
+
+    function getDebt(address _borrower) public view returns (uint256) {
+        return debts[msg.sender][_borrower];
+    }
+}
---
+++ 0X+1_Mappings/README.md
@@ -0,0 +1,5 @@
+# Smart Contract Developer Bootcamp - İTÜ Blockchain Devs
+
+Bu dersimizde Mapping veri tipini inceledik.
+
+[If Else Koşul Yapıları Kontrat Örneği](./Mapping.sol)
