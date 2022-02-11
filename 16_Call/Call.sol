383eda3748b4e683eb3d3f94d7606eb2bb7bab22
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-02-11 22:39:40 +0300

    Call

---
+++ 16_Call/Call.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract Test {
+    uint256 public total = 0;
+    uint256 public fallbackCalled = 0;
+    string public incrementer;
+
+    fallback() external payable {
+        fallbackCalled += 1;
+    }
+
+    function inc(uint256 _amount, string memory _incrementer) external returns(uint256) {
+        total += _amount;
+        incrementer = _incrementer;
+
+        return total;
+    } 
+}
+
+contract Caller {
+    
+    function testCall(address _contract, uint256 _amount, string memory _incrementer) external returns (bool, uint256) {
+       (bool err, bytes memory res) = _contract.call(abi.encodeWithSignature("inc(uint256, string)", _amount, _incrementer));
+        uint256 _total = abi.decode(res, (uint256));
+        return (err, _total);
+    }
+
+    function payToFallback(address _contract) external payable {
+        (bool err,) = _contract.call{value: msg.value}("");
+
+        if(!err) revert();
+    }
+
+
+}
--- 16_Call/README.md
+++ 16_Call/README.md
@@ -1,5 +1,7 @@
 # Smart Contract Developer Bootcamp - İTÜ Blockchain Devs
 
-...
+Bu dersimizde "call" methodu aracılığıyla low-level çağrılar yapma konusunu inceledik.
 
-[](./.sol)
+[Video İçeriği](https://www.youtube.com/playlist?list=PLby2HXktGwN4Cof_6a8YwlMrboX8-hs73)
+
+[Call Methodu Kontrat Örneği](./Call.sol)
