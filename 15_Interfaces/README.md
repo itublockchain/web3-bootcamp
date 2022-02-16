8c320a527a15a1baff479d4114df9a82794ab2e8
Zetsuboii <hmzkrb@gmail.com>
2022-02-17 00:39:30 +0300

    add 15_Interface contracts + README

--- 15_Interfaces/README.md
+++ 15_Interfaces/README.md
@@ -1,5 +1,18 @@
 # Smart Contract Developer Bootcamp - İTÜ Blockchain Devs
 
-...
+Bu dersimizde Interfaces konusunu işledik.
 
-[](./.sol)
+Interface'ler (Arayüzler) çalışma mantığı farklı olan ama yaptığı iş aynı olan kontratların (örneğin token kontratları) ortak bir standarda sahip olmasını böylece bu kontratlarla çalışmak isteyen birinin her bir kontrata özgü kod yazmak yerine bu standarda uygun tek bir kod yazmasını sağlar
+
+ERC20, ERC721, ERC1155 gibi standartlar aslında bir interface şeklinde tanımlanmıştır.
+
+bkz. [IERC20 standardı](https://eips.ethereum.org/EIPS/eip-20)
+bkz. [IERC20 interface](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol)
+
+[Video İçeriği]()
+
+[Interfaces](./Interfaces.sol)
+[Vault](./Vault.sol)
+[IToken](./IToken.sol)
+[TokenA](./TokenA.sol)
+[TokenB](./TokenB.sol)
---
+++ 15_Interfaces/TokenA.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./IToken.sol";
+
+contract TokenA is IToken {
+    uint256 constant private OWNER_BALANCE = 1000 * 10**18;
+
+    mapping(address => uint256) balances;
+
+    // Allower -> Allowee -> Amount
+    mapping(address => mapping(address => uint256)) allowances;
+
+    constructor() {
+        balances[msg.sender] = OWNER_BALANCE;
+    }
+
+    function transfer(address recipient, uint256 amount) external override {
+        require(recipient != address(0), "XFER_TO_ZERO");
+        require(balances[msg.sender] >= amount, "INSUFFICENT_BAL");
+    
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+    } 
+
+    function transferFrom(address from, address to, uint256 amount) external override {
+        require(to != address(0), "XFER_TO_ZERO");
+        require(allowances[from][msg.sender] >= amount, "INSUFFICIENT_ALLOWANCE");
+        require(balances[from] >= amount, "INSUFFICENT_BALANCE");
+
+        allowances[from][msg.sender] -= amount;
+        balances[from] -= amount;
+        balances[to] += amount;
+    }
+
+    function increaseAllowance(address recipient, uint256 amount) external {
+        require(recipient != address(0), "APPR_TO_ZERO");
+        allowances[msg.sender][recipient] += amount;
+    }
+
+    function decreaseAllowance(address recipient, uint256 amount) external {
+        require(recipient != address(0), "APPR_TO_ZERO");
+        allowances[msg.sender][recipient] -= amount;
+    }
+
+    function balanceOf(address user) external view override returns(uint256) {
+        return balances[user];
+    }
+}
