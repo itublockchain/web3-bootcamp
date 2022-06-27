a90210c46ce5b16c5dba6014767c626e27311aca
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-06-27 20:00:48 +0300

    Patika.dev complete code

---
+++ 7x0_PatikaLendingWorkshop/USD.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDolar is ERC20, Ownable {
+    constructor() ERC20("US Dolar", "USD") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
