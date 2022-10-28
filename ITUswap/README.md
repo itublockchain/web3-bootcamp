05eff4ad2000c4e43e89ff6aced8165a83963fe7
tahos81 <tahirozpala@gmail.com>
2022-10-28 16:21:18 +0300

    ITUswap

---
+++ ITUswap/README.md
@@ -0,0 +1,3 @@
+Building a dex from scratch
+
+Source: https://jeiwan.net/posts/programming-defi-uniswap-1/
---
+++ ITUswap/contracts/Exchange.sol
@@ -0,0 +1,65 @@
+//SPDX-License-Identifier: None
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract Exchange {
+    address public tokenAddress;
+
+    constructor(address _token) {
+        require(_token != address(0), "invalid token address");
+        
+        tokenAddress = _token;
+    }
+
+    function addLiquidity(uint256 _tokenAmount) public payable {
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
+
+    function ethToTokenSwap(uint256 _minToken) public payable {
+        uint256 tokenReserve = getReserve();
+        uint256 tokensBought = getAmount(msg.value, address(this).balance - msg.value, tokenReserve);
+
+        require(tokensBought >= _minToken, "insufficient output ampount");
+
+        IERC20(tokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    function tokenToEthSwap(uint256 _tokensSold, uint256 _minEth) public {
+        uint256 tokenReserve = getReserve();
+        uint256 ethBought = getAmount(_tokensSold, tokenReserve, address(this).balance);
+
+        require(ethBought >= _minEth, "insufficent output amount");
+
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokensSold);
+        payable(msg.sender).transfer(ethBought);
+    }
+
+    function getReserve() public view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+        require(_ethSold > 0);
+
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
+        require(_tokenSold > 0);
+
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
+    }
+
+    function getAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) private pure returns(uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        return (inputReserve * outputReserve) / (inputReserve + inputAmount);
+    }
+}
+
